adccde00835e1e58a05ed75084880cab1c26eb4b
ayushi0004 <ayushichaudhari404@gmail.com>
2024-08-30 21:30:11 +0530

    seller

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,15 +22,18 @@ from main import views as mv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #customer
-    path('login/customer',mv.customer_login_view, name='customer_login'),
-    path('register/customer',mv.customer_register_view, name='customer_register'),
-    path('forgot/customer',mv.customer_forgot_pass_view, name='customer_forgot_pass'),
-    #seller
-    path('login/seller',mv.seller_login_view, name='seller_login'),
-    path('register/seller',mv.seller_register_view, name='seller_register'),
-    path('forgot/seller',mv.seller_forgot_pass_view, name='seller_forgot_pass'),
-    path('',mv.home_view, name='home'),
+    # customer
+    path('login/customer', mv.customer_login_view, name='customer_login'),
+    path('register/customer', mv.customer_register_view, name='customer_register'),
+    path('forgot/customer', mv.customer_forgot_pass_view, name='customer_forgot_pass'),
+    # seller
+    path('login/seller', mv.seller_login_view, name='seller_login'),
+    path('register/seller', mv.seller_register_view, name='seller_register'),
+    path('forgot/seller', mv.seller_forgot_pass_view, name='seller_forgot_pass'),
+    # common to both
+    path('logout', mv.logout_view, name='logout'),
+    # index
+    path('', mv.home_view, name='home'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
--- main/views.py
+++ main/views.py
@@ -10,21 +10,42 @@ def logout_view(request):
 def home_view(request):
     return render(request, 'home.html')
 
-#seller views
+# seller views
 def seller_login_view(request):
-    return render(request, 'account/seller/login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        error = False
+        if not username :
+            messages.error(request, 'Username is required')
+            error = True
+        if not password:  
+            messages.error(request, 'Password is required') 
+            error = True
+        if not error:
+            # check if user exists
+            user = authenticate(username=username, password=password)
+            if not user:
+                messages.error(request, 'Invalid username or password')
+            else:
+                # check if user is a seller
+                if user.groups.filter(name='seller').exists():
+                    login(request, user)
+                    messages.success(request, 'You are now logged in as Seller')
+                    return redirect('home')
+                else:
+                    messages.error(request, 'You are not registered as a seller')
+    return render(request, 'accounts/seller/login.html')
 def seller_register_view(request):
-    return render(request, 'account/seller/register.html')
+    return render(request, 'accounts/seller/register.html')
 def seller_forgot_pass_view(request):
-    return render(request, 'account/seller/forgot_password.html')
+    return render(request, 'accounts/seller/forgot_password.html')
 
-#customer views
+# customer views
 def customer_login_view(request):
-    return render(request, 'account/customer/login.html')
+    return render(request, 'accounts/customer/login.html')
 def customer_register_view(request):
-    return render(request, 'account/customer/register.html')
+    return render(request, 'accounts/customer/register.html')
 def customer_forgot_pass_view(request):
-    return render(request, 'account/customer/forgot_password.html')
-
-
-
+    return render(request, 'accounts/customer/forgot_password.html')
